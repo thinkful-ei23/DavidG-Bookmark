107213359442bf217907982353540a3a00c74244
David Graves <davidgraves831@gmail.com>
2018-07-26 16:12:35 -0500

    added some brackets

---
+++ scripts/api.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const api = (function(){
+
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/davidg';
+
+  const getBookmarks = function(){
+
+  };
+
+  const creatBookmark = function(){
+
+  };
+
+
+  return {
+    getBookmarks,
+    creatBookmark
+  };
+
+
+
+});
---
+++ scripts/store.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const store = (function(){
+
+  let bookmarks = [];
+
+  const findById = function() {
+    
+  };
+
+  const addBookmark = function(){
+
+  };
+
+  const findAndDelete = function(){
+
+  };
+
+  const toggleRatingFilter = function(){
+
+  };
+
+  return {bookmarks, findById, addBookmark, findAndDelete, toggleRatingFilter};
+
+
+}());
